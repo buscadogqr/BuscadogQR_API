ce7d373fdc91952f0b4e7c5da6dda3616fd29c9d
vickyraineriUADE <vraineri@uade.edu.ar>
2025-01-27 14:15:34 -0300

    hopefully works

--- api/src/db.js
+++ api/src/db.js
@@ -3,37 +3,39 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT, NODE_ENV,
 } = process.env;
 
-let sequelize =
-	process.env.NODE_ENV === 'production'
-		? new Sequelize({
-				database: DB_NAME,
-				dialect: 'postgres',
-				host: DB_HOST,
-				port: 5432,
-				username: DB_USER,
-				password: DB_PASSWORD,
-				pool: {
-					max: 3,
-					min: 1,
-					idle: 10000,
-				},
-				dialectOptions: {
-					ssl: {
-						require: true,
-						// Ref.: https://github.com/brianc/node-postgres/issues/2009
-						rejectUnauthorized: false,
-					},
-					keepAlive: true,
-				},
-				ssl: true,
-		  })
-		: new Sequelize(
-				`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-				{ logging: false, native: false }
-		  );
+let sequelize;
+
+if (NODE_ENV === 'production') {
+  sequelize = new Sequelize({
+    database: DB_NAME,
+    username: DB_USER,
+    password: DB_PASSWORD,
+    host: DB_HOST,
+    port: DB_PORT || 5432,
+    dialect: 'postgres',
+    pool: {
+      max: 3,
+      min: 1,
+      idle: 10000,
+    },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Render requiere esto.
+      },
+      keepAlive: true,
+    },
+    ssl: true,
+  });
+} else {
+  sequelize = new Sequelize(
+    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+    { logging: false, native: false }
+  );
+}
 
 const basename = path.basename(__filename);
 
@@ -53,17 +55,8 @@ let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
 sequelize.models = Object.fromEntries(capsEntries);
 
-// En sequelize.models están todos los modelos importados como propiedades
-// Para relacionarlos hacemos un destructuring
-// const { Users, Pets } = sequelize.models;
-
-// Aca vendrian las relaciones
-
-// Users.hasMany(Pets);
-// Pets.belongsTo(Users);
-
-
+// Exportamos los modelos y la conexión
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-  conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
+  conn: sequelize,     // para importar la conexión { conn } = require('./db.js');
 };
